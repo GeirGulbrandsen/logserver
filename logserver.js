e13d13b78a6830b7017ff83b72f231e1c23b030c
GeirGulbrandsen <dobbelg@gmail.com>
2019-11-08 14:15:44 +0000

    Formatting

--- logserver.js
+++ logserver.js
@@ -3,16 +3,17 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // Body parser use JSON data
 
-app.post('/logs', function(req, res) {
+app.post('/logs', function (req, res) {
 
-	    var outputFilename = './logs.json'; // path of the file to output
+    var outputFilename = './logs.json'; // path of the file to output
 
-	    fs.appendFileSync(outputFilename, JSON.stringify(JSON.parse(req.body.payload), null, 4)+"\n"); // write to the file system
 
-	    res.send('Saved to ' + outputFilename);
+    fs.appendFileSync(outputFilename, JSON.stringify(JSON.parse(req.body.payload), null, 4) + "\n"); // write to the file system
+
+    res.send('Saved to ' + outputFilename);
 
 });
 
